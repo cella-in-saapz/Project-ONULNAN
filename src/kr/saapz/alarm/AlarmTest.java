29b6a9810cc4f2a03dbcad7a28998bd1a2aacdd8
JUNSU <cella@saapz.kr>
2012-11-02 00:27:09 +0900

    time

--- src/kr/saapz/alarm/AlarmTest.java
+++ src/kr/saapz/alarm/AlarmTest.java
@@ -22,12 +22,14 @@ public class AlarmTest extends Activity {
 				// TODO Auto-generated method stub
 				AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
 				Intent intent = new Intent(AlarmTest.this, AlarmReceiver.class);
-				PendingIntent sender = PendingIntent.getBroadcast(AlarmTest.this, 0, intent, 0);
+				PendingIntent sender = PendingIntent.getBroadcast(AlarmTest.this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 				
 				Calendar calendar = Calendar.getInstance();
-				calendar.setTimeInMillis(System.currentTimeMillis());
-				calendar.add(Calendar.SECOND, 10);
-				am.set(AlarmManager.RTC, calendar.getTimeInMillis(), sender);
+				calendar.set(Calendar.HOUR_OF_DAY, 9);
+				calendar.set(Calendar.MINUTE, 0);
+				calendar.set(Calendar.SECOND, 0);
+				
+				am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, sender);
 				
 			}
 		});
